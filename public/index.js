049c3e772fe78f359ece29d8214194f40e2ac4fa
Alexander Betke <main@alexinabox.de>
2025-06-16 23:13:09 +0200

    initial commit

---
+++ public/index.js
@@ -0,0 +1,14 @@
+const container = document.getElementById("starBackground");
+const numStars = 250; // Reduced from 200
+
+for (let i = 0; i < numStars; i++) {
+  const scale = Math.random() * 2 + 1;
+  const star = document.createElement("div");
+  star.className = "star";
+  star.style.left = Math.random() * 100 + "%";
+  star.style.top = Math.random() * 100 + "%";
+  star.style.width = scale + "px";
+  star.style.height = scale + "px";
+  star.style.animationDelay = Math.random() * 3 + "s";
+  container.appendChild(star);
+}
