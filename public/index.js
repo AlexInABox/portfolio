101c5e889d0a43424dd5b2ae34bcc3e13b65c1e6
AlexInABox <hobby@alexinabox.de>
2026-02-23 13:32:59 +0100

    feat: rewrote starry background to be a canvas

--- public/index.js
+++ public/index.js
@@ -1,83 +1,92 @@
-const container = document.getElementById("starBackground");
-const numStars = 200;
+const container = document.getElementById('starBackground');
+const ctx = container.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
 
-let mouseX = -100;
-let mouseY = -100;
-this.addEventListener("mousemove", storeMousePos);
-function storeMousePos(e) {
-  mouseX = e.pageX;
-  mouseY = e.pageY;
-}
+let mouseX = 0;
+let mouseY = 0;
+let mouseDown = false;
 
-//star moving directions
-upOrDown = Math.round(Math.random()) * 2 - 1;
-leftOrRight = (Math.random() - 0.5) * 2;
+const numStars = 1500;
+let listOfLayer1Stars = [[0, 0]];
+let listOfLayer2Stars = [[0, 0]];
+let listOfLayer3Stars = [[0, 0]];
+const layer1Size = 0.5;
+const layer2Size = 1;
+const layer3Size = 1.5;
 
-function populateStarsWithClassName(className) {
-  for (let i = 0; i < numStars; i++) {
-    const starScale = Math.random() * 2 + 1;
-    const detectorScale = 90;
-    const star = document.createElement("div");
-    const detector = document.createElement("div");
+function initializeBackground() {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  listOfLayer1Stars = [[0, 0]];
+  listOfLayer2Stars = [[0, 0]];
+  listOfLayer3Stars = [[0, 0]];
 
-    star.className = "star " + className;
-    star.style.left = Math.random() * 110 + "%";
-    star.style.top = Math.random() * 110 + "%";
-    star.style.width = starScale + "px";
-    star.style.height = starScale + "px";
-    star.style.animationDelay = Math.random() * 3 + "s";
-    container.appendChild(star);
+  function place(layer, size) {
+    for (let i = 0; i < numStars / 3; i++) {
+      const x = (Math.random() * ctx.canvas.width) % ctx.canvas.width;
+      const y = (Math.random() * ctx.canvas.height) % ctx.canvas.height;
+      ctx.beginPath();
+      ctx.arc(x, y, size, 0, 2 * Math.PI);
+      ctx.fillStyle = 'white';
+      ctx.fill();
 
-    detector.className = "detector";
-    detector.style.width = detectorScale + "px";
-    detector.style.height = detectorScale + "px";
-
-    star.appendChild(detector);
+      layer[layer.length] = [x, y];
+    }
   }
+  place(listOfLayer1Stars, layer1Size);
+  place(listOfLayer2Stars, layer2Size);
+  place(listOfLayer3Stars, layer3Size);
 }
+initializeBackground();
 
-function moveStarsByLayerAndSpeed(layer, speed) {
-  var stars = document.getElementsByClassName(layer);
-  for (const star of stars) {
-    let horizontal = parseFloat(star.style.left.slice(0, -1));
-    let vertical = parseFloat(star.style.top.slice(0, -1));
-    horizontal = (horizontal + speed * leftOrRight + 110) % 110;
-    vertical = (vertical + speed * upOrDown + 110) % 110;
-    star.style.left = horizontal + "%";
-    star.style.top = vertical + "%";
+function moveStars() {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  function move(layer, speed, size) {
+    for (i = 0; i < layer.length; i++) {
+      let [x, y] = layer[i];
+
+      if (x - mouseX > -50 && x - mouseX < 50 && y - mouseY > -50 && y - mouseY < 50 && mouseDown) {
+        x = x - (x - mouseX) / 20;
+        y = y - (y - mouseY) / 20;
+      } else {
+        x = (x + speed) % ctx.canvas.width;
+        y = (y + speed) % ctx.canvas.height;
+      }
+
+      ctx.beginPath();
+      ctx.arc(x, y, size, 0, 2 * Math.PI);
+      ctx.fillStyle = 'white';
+      ctx.fill();
+      layer[i] = [x, y];
+    }
   }
+
+  move(listOfLayer1Stars, 0.25, layer1Size);
+  move(listOfLayer2Stars, 0.45, layer2Size);
+  move(listOfLayer3Stars, 0.65, layer3Size);
 }
-populateStarsWithClassName("layer1");
-populateStarsWithClassName("layer2");
-populateStarsWithClassName("layer3");
+setInterval(() => moveStars(), 10);
 
-setInterval(() => moveStarsByLayerAndSpeed("layer1", 0.1), 100);
-setInterval(() => moveStarsByLayerAndSpeed("layer2", 0.05), 100);
-setInterval(() => moveStarsByLayerAndSpeed("layer3", 0.02), 100);
+addEventListener('resize', (_) => {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
 
-setInterval(() => pushStarsAway(), 10);
+  initializeBackground();
+});
 
-function pushStarsAway() {
-  const _overlapped = document.elementsFromPoint(mouseX, mouseY);
-  const _included = _overlapped.filter((el) => el.className == "detector");
+addEventListener('mousemove', function (e) {
+  function getMousePosition(canvas, event) {
+    let rect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
+  }
+  getMousePosition(container, e);
+});
 
-  _included.forEach((element) => {
-    let rect = element.parentElement.getBoundingClientRect();
-    let centerX = rect.left + rect.width / 2;
-    let centerY = rect.top + rect.height / 2;
-
-    let dx = mouseX - centerX;
-    let dy = mouseY - centerY;
-
-    let dist = Math.sqrt(dx * dx + dy * dy);
-    let factor = 100 / dist;
-
-    let newX = rect.left - dx * factor * 0.03;
-    let newY = rect.top - dy * factor * 0.03;
-
-    element.parentElement.style.left =
-      (newX / visualViewport.width) * 100 + "%";
-    element.parentElement.style.top =
-      (newY / visualViewport.height) * 100 + "%";
-  });
-}
+document.body.onmousedown = function () {
+  mouseDown = true;
+};
+document.body.onmouseup = function () {
+  mouseDown = false;
+};
